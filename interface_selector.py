8f4fa78ad36bedad085dfb701b55bb5930c40f66
atcrowe17 <tyc17afguy@gmail.com>
2022-06-17 05:34:18 -0400

    Tested new logic for tagging interface types. No doubles tagging. Added definitions for XE and XR tunnel and l3 interfaces.

--- interface_selector.py
+++ interface_selector.py
@@ -8,6 +8,19 @@ import os
 def add_to_interface_dict(interface):
     interface_dict['interface_table'].append(interface)
 
+def id_software_ver(open_config_file):
+    re_sw_xr = re.compile(r'(^.*IOS XR.*\n)', re.M)
+    execute_re = re_sw_xr.finditer(open_config_file)
+    # result =
+    # re_get_sw = re.compile(r'^!! IOS XR.*', re.M).search(execute_re)
+    # result_value = result.group
+    for result in execute_re:
+        result_value = result.group()
+        if "IOS XR" in result_value:
+            return('xr')
+        else:
+            return('xe')
+
 #Takes a Cisco config file and creates a file called re_results.txt with just the interface configs
 def ints_from_config (config_file):
     file_open = open(config_file)
@@ -15,118 +28,152 @@ def ints_from_config (config_file):
     #matches interface configuration sections
     re_get_ints = re.compile(r'(?<=!\n)(interface.*\n)(.*\n)*?(?=!)', re.M)
     read_config = file_open.read()
+    sw_ver = id_software_ver(read_config)
     execute_re = re_get_ints.finditer(read_config)
-    for result in execute_re:
-        result_value = result.group()
-        re_int_label = re.compile(r'interface.*').match(result_value)
-        re_shut_int = re.compile(r'^.shutdown.*', re.M).search(result_value)
-        re_l3_vlan_int = re.compile(r'interface Vlan.*', re.M).search(result_value)
-        re_access_int = re.compile(r'^.switchport mode access.*', re.M).search(result_value)
-        re_trunk_int = re.compile(r'^.switchport mode trunk.*', re.M).search(result_value)
-        re_l3_xe_int = re.compile(r'^.ip address.*', re.M).search(result_value)
-        re_l3_xr_int = re.compile(r'^.ipv4 address.*', re.M).search(result_value)
-        re_no_ip_xe_int = re.compile(r'^.no switchport.*', re.M).search(result_value)
-        re_loop_int = re.compile(r'^interface Loopback.*', re.M).search(result_value)
-        re_tun_xr_int = re.compile(r'^interface tunnel-ip.*', re.M).search(result_value)
-        re_tun_xe_int = re.compile(r'^interface Tunnel.*', re.M).search(result_value)
-        if re_shut_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "shutdown"
-        #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_l3_vlan_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l3_vlan"
+    print(sw_ver)
+    if sw_ver == 'xr':
+        for result in execute_re:
+            result_value = result.group()
+            re_int_label = re.compile(r'interface.*').match(result_value)
+            re_shut_xr_int = re.compile(r'^.shutdown.*', re.M).search(result_value)
+            re_l3_xr_int = re.compile(r'^.ipv4 address.*', re.M).search(result_value)
+            re_loop_xr_int = re.compile(r'^interface Loopback.*', re.M).search(result_value)
+            re_tun_xr_int = re.compile(r'^interface tunnel-ip.*', re.M).search(result_value)
+            if re_shut_xr_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "shut_xr"
             #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_access_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l2_access"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_loop_xr_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "loop_xr"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_tun_xr_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "tun_xr"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_l3_xr_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l3_xr"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            else:
+                pass
+    else:
+        for result in execute_re:
+            result_value = result.group()
+            re_int_label = re.compile(r'interface.*').match(result_value)
+            re_shut_int = re.compile(r'^.shutdown.*', re.M).search(result_value)
+            re_l3_vlan_int = re.compile(r'interface Vlan.*', re.M).search(result_value)
+            re_access_int = re.compile(r'^.switchport mode access.*', re.M).search(result_value)
+            re_trunk_int = re.compile(r'^.switchport mode trunk.*', re.M).search(result_value)
+            re_l3_xe_int = re.compile(r'^.ip address.*', re.M).search(result_value)
+            re_no_ip_xe_int = re.compile(r'^.no switchport.*', re.M).search(result_value)
+            re_loop_xe_int = re.compile(r'^interface Loopback.*', re.M).search(result_value)
+            re_tun_xe_int = re.compile(r'^interface Tunnel.*', re.M).search(result_value)
+            if re_shut_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "shutdown"
             #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_trunk_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l2_dot1q"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_loop_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "loopback"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_tun_xe_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "tun_xe"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_tun_xr_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "tun_xr"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_l3_xe_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l3_xe"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_l3_xr_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l3_xr"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        elif re_no_ip_xe_int != None:
-            interface = {}
-            interface['label'] = re_int_label.group()
-            interface['type'] = "l3_no_ip"
-            #            interface['location'] = "internal" "external"
-            print(interface)
-            add_to_interface_dict(interface)
-        else:
-            pass
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_l3_vlan_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l3_vlan"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_access_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l2_access"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_trunk_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l2_dot1q"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_loop_xe_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "loop_xe"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_tun_xe_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "tun_xe"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_l3_xe_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l3_xe"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            elif re_no_ip_xe_int != None:
+                interface = {}
+                interface['label'] = re_int_label.group()
+                interface['type'] = "l3_no_ip"
+                #            interface['location'] = "internal" "external"
+                print(interface)
+                add_to_interface_dict(interface)
+            else:
+                pass
 
-def config_gen_shutdown (shutdown_int):
-    shutdown_config_list = ["default " + shutdown_int['label'] + "\n", shutdown_int['label'] + "\n", " description <== DISABLED ==>\n",
-                            " switchport access vlan 2\n", " switchport mode access\n",
-                            " no logging event link-status\n", " shutdown\n",
+def config_gen_shut_xe (shut_xe_int):
+    shut_xe_config_list = ["default " + shut_xe_int['label'] + "\n", shut_xe_int['label'] + "\n",
+                            " description <== DISABLED ==>\n", " switchport access vlan 2\n",
+                            " switchport mode access\n", " no logging event link-status\n", " shutdown\n",
                             " no cdp enable\n", " no snmp trap link-status\n", "!\n"]
-    return(shutdown_config_list)
+    return(shut_xe_config_list)
+
+def config_gen_shut_xr (shut_xr_int):
+    shut_xr_config_list = [shut_xr_int['label'] + "\n", " description <== DISABLED ==>\n",
+                           " ipv4 unreachables disable\n", " no ipv4 directed-broadcast\n",
+                           " no ipv4 mask-reply\n", " no ipv4 redirects\n", " lldp transmit disable\n",
+                           " lldp receive disable\n", " shutdown\n", "!\n"]
+    return(shut_xr_config_list)
 
 def config_gen_l2_access (l2_access_int):
-    l2_access_config_list = [l2_access_int['label'] + "\n", " spanning-tree bpduguard enable\n", "!\n"]
+    l2_access_config_list = [l2_access_int['label'] + "\n", " no cdp enable\n", " no lldp receive\n",
+                             " no lldp transmist\n", "\n", " spanning-tree portfast edge\n", "!\n"]
     return(l2_access_config_list)
 
 def config_gen_l2_dot1q (l2_dot1q_int):
-    l2_dot1q_config_list = [l2_dot1q_int['label'] + "\n", " spanning-tree guard root\n", "!\n"]
+    l2_dot1q_config_list = [l2_dot1q_int['label'] + "\n", " no storm-control unicast\n",
+                            " no storm-control broadcast\n", " no spanning-tree guard root\n",
+                            " no spanning-tree portfast\n", "!\n"]
     return(l2_dot1q_config_list)
 
 def config_gen_ints_xe_l3(xe_l3_int):
-    xe_l3_config_list = [xe_l3_int['label'] + "\n", " #this should be a XE interface\n", " no ip unreachables\n",
-                         " no ip mask-reply\n", " no ip redirects\n", " no ip proxy-arp\n",
-                         " no lldp transmit\n", "!\n"]
+    xe_l3_config_list = [xe_l3_int['label'] + "\n", " no ip mask-reply\n",
+                         " no ip redirects\n", " no ip proxy-arp\n", " no lldp transmit\n", " no lldp receive\n", "!\n"]
     return(xe_l3_config_list)
 
 def config_gen_ints_xr_l3(xr_l3_int):
-    xr_l3_config_list = [xr_l3_int['label'] + "\n", " #this should be a XR interface\n", " no ip unreachables\n",
-                         " no ip mask-reply\n", " no ip redirects\n", " no ip proxy-arp\n",
-                         " no lldp transmit\n", "!\n"]
+    xr_l3_config_list = [xr_l3_int['label'] + "\n", " ipv4 unreachables disable\n", " no ipv4 directed-broadcast\n",
+                         " no ipv4 mask-reply\n", " no ipv4 redirects\n", " lldp transmit disable\n",
+                         " lldp receive disable\n", "!\n"]
     return(xr_l3_config_list)
 
 def config_gen_ints_l3_vlan(l3_vlan_int):
@@ -154,19 +201,27 @@ def config_gen_ints_tun_xr(tun_xr_int):
                           " no ip proxy-arp\n", " no lldp transmit\n", "!\n"]
     return(tun_xr_config_list)
 
-def config_gen_ints_loopback(loopback_int):
-    loopback_config_list = [loopback_int['label'] + "\n", " no ip directed-broadcast\n", " no ip unreachables\n",
+def config_gen_ints_loop_xe(loop_xe_int):
+    loop_xe_config_list = [loop_xe_int['label'] + "\n", " no ip directed-broadcast\n", " no ip unreachables\n",
                             " no ip mask-reply\n", " no ip redirects\n", " no ip proxy-arp\n", "!\n"]
-    return(loopback_config_list)
+    return(loop_xe_config_list)
+
+def config_gen_ints_loop_xr(loop_xr_int):
+    loop_xr_config_list = [loop_xr_int['label'] + "\n", " no ip directed-broadcast\n", " no ip unreachables\n",
+                            " no ip mask-reply\n", " no ip redirects\n", " no ip proxy-arp\n", "!\n"]
+    return(loop_xr_config_list)
 
 def config_gen_ints(ints_to_gen, int_config_file):
     directory = "./configs/created/"
     int_config = open(directory + filename, 'w')
     int_config.writelines("!\n")
     for interface in interface_dict['interface_table']:
-        if interface['type'] == "shutdown":
-            shutdown_int_config = config_gen_shutdown(interface)
-            int_config.writelines(shutdown_int_config)
+        if interface['type'] == "shut_xe":
+            shut_xe_int_config = config_gen_shut_xe(interface)
+            int_config.writelines(shut_xe_int_config)
+        elif interface['type'] == "shut_xr":
+            shut_xr_int_config = config_gen_shut_xr(interface)
+            int_config.writelines(shut_xr_int_config)
         elif interface['type'] == "l2_access":
             l2_access_int_config = config_gen_l2_access(interface)
             int_config.writelines(l2_access_int_config)
@@ -185,9 +240,12 @@ def config_gen_ints(ints_to_gen, int_config_file):
         elif interface['type'] == "tun_xr":
             tun_xr_int_config = config_gen_ints_tun_xr(interface)
             int_config.writelines(tun_xr_int_config)
-        elif interface['type'] == "loopback":
-            loopback_int_config = config_gen_ints_loopback(interface)
-            int_config.writelines(loopback_int_config)
+        elif interface['type'] == "loop_xe":
+            loop_xe_int_config = config_gen_ints_loop_xe(interface)
+            int_config.writelines(loop_xe_int_config)
+        elif interface['type'] == "loop_xr":
+            loop_xr_int_config = config_gen_ints_loop_xr(interface)
+            int_config.writelines(loop_xr_int_config)
         elif interface['type'] == "l3_xe":
             xe_l3_int_config = config_gen_ints_xe_l3(interface)
             int_config.writelines(xe_l3_int_config)
@@ -208,8 +266,8 @@ for file in os.listdir(directory):
 
 
 
-def lisp_int():
-    return(None)
+# def lisp_int():
+#     return(None)
 
 
 save_results = open('interface_table.txt', 'w')
